a090f6e59115abea69a92e0d000646b5c8bce6b4
Linca <linca1978@gmail.com>
2019-04-17 08:33:11 +0300

    first commit

---
+++ config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+    url : "mongodb://linca:12345@notes-shard-00-00-jjzis.gcp.mongodb.net:27017,notes-shard-00-01-jjzis.gcp.mongodb.net:27017,notes-shard-00-02-jjzis.gcp.mongodb.net:27017/test?ssl=true&replicaSet=notes-shard-0&authSource=admin&retryWrites=true",
+
+};
