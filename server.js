0765237fc18d75357829281ca29693ea9b770fc3
Linca <linca1978@gmail.com>
2019-04-22 11:21:05 +0300

    add new direc

--- server.js
+++ server.js
@@ -4,11 +4,34 @@ const bodyParser     = require('body-parser');
 const path = require('path');
 
 const db             = require('./config/db');
+const mongoose = require('mongoose');
 const app            = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+mongoose.Promise = global.Promise;
+
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+});
+
+require('./app/routes/note.routes.js')(app);
+
+// listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
 
 MongoClient.connect(db.url, (err, database) => {
     if (err) return console.log(err)
