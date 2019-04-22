8aa7b2050e50306d0b22cfc22a56c4d2fd67e2e5
Linca <linca1978@gmail.com>
2019-04-22 11:37:05 +0300

    add new file

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 const express        = require('express');
-const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const path = require('path');
-
 const db             = require('./config/db');
 const mongoose = require('mongoose');
 const app            = express();
-const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')));
 mongoose.Promise = global.Promise;
 
-mongoose.connect(dbConfig.url, {
+mongoose.connect(db.url, {
     useNewUrlParser: true
 }).then(() => {
     console.log("Successfully connected to the database");
@@ -26,18 +24,11 @@ app.get('/', (req, res) => {
     res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
 });
 
-require('./app/routes/note.routes.js')(app);
+require('./app/routes/note_routes.js')(app);
 
 // listen for requests
 app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+    console.log('We are live on ' + 3000);
+    // console.log('Connected to '+ db.url);
 });
 
-MongoClient.connect(db.url, (err, database) => {
-    if (err) return console.log(err)
-    require('./app/routes')(app, database);
-    app.listen(port, () => {
-        console.log('We are live on ' + port);
-        console.log('Connected to '+ db.url);
-    });
-})
